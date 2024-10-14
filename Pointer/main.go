c966cfb27c84b1aa7062845ad7ad1c2325f21b32
Saksham Bhayana <saksham@Sakshams-MacBook-Pro.local>
2024-10-14 11:49:52 +0530

    Added comments to pointer code

--- Pointer/main.go
+++ Pointer/main.go
@@ -6,12 +6,12 @@ func main() {
  name := "bill"
  
  namePointer := &name
- fmt.Println(namePointer)
- fmt.Println(&namePointer)
- printPointer(namePointer) 
+ fmt.Println(namePointer)  // Output: 0xc000010230 (memory address of 'name')
+ fmt.Println(&namePointer) // Output: 0xc00000e028 (memory address of 'namePointer')
+ printPointer(namePointer)
 }
  
 func printPointer(namePointer *string) {
- fmt.Println(namePointer)
- fmt.Println(&namePointer)
+ fmt.Println(namePointer)  // Output: 0xc000010230 (same as in main())
+ fmt.Println(&namePointer) // Output: 0xc00000e038 (different address, local to the function)
 }
