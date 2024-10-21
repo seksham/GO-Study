bceff0ba9753bf63bcdb499987fc7d96f64ff3da
Saksham Bhayana <saksham@Sakshams-MacBook-Pro.local>
2024-10-21 13:47:50 +0530

    Added a comment

--- concurrencypatterns/generator/main.go
+++ concurrencypatterns/generator/main.go
@@ -14,10 +14,10 @@ func repeaterFunc[T any, K any](done <-chan T, fn func() K) <-chan K {
 		defer close(stream)
 		for {
 			select {
-			case <- done:
+			case <-done:
 				return // Exit the goroutine when done signal is received
 			default:
-				stream <- fn() // Call the provided function and send result to stream
+				stream <- fn() // Call the provided function and send result to stream.  case stream <- fn():  is more effecient as it is back pressure aware and sends only when channel is ready
 			}
 		}
 	}()
@@ -26,16 +26,16 @@ func repeaterFunc[T any, K any](done <-chan T, fn func() K) <-chan K {
 
 func main() {
 	done := make(chan bool)
-	
+
 	// Define a function that generates random numbers between 0 and 9
 	rondomNumber := func() int {
 		return rand.Intn(10)
 	}
-	
+
 	count := 0
 	// Use repeaterFunc to generate a stream of random numbers
 	for num := range repeaterFunc(done, rondomNumber) {
-		time.Sleep(time.Second/4) // Pause for 250ms between numbers
+		time.Sleep(time.Second / 4) // Pause for 250ms between numbers
 		fmt.Println(num)
 		count++
 		if count == 20 {
